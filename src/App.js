261e0b72d111575d1669c54d7a7b70b38aac3d32
brians123 <brianssui@yahoo.com>
2020-04-18 23:26:27 -0400

    styled static page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import './App.css';
+
+// rbx styling
+import "rbx/index.css";
+import { Navbar, Button, Column} from "rbx";
+
+// Components
+import Product from './components/Product';
 
 const App = () => {
   const [data, setData] = useState({});
@@ -13,9 +21,66 @@ const App = () => {
   }, []);
 
   return (
-    <ul>
-      {products.map(product => <li key={product.sku}>{product.title}</li>)}
-    </ul>
+   <div>
+     <Navbar color="light">
+     <Navbar.Brand>
+    <Navbar.Item href="#">
+      <img
+        src="https://cdn.businessoffashion.com/brand/bof-logo.svg"
+        alt=""
+        role="presentation"
+        width="112"
+        height="28"
+      />
+    </Navbar.Item>
+    <Navbar.Burger />
+  </Navbar.Brand>
+  <Navbar.Menu>
+    <Navbar.Segment align="start">
+      <Navbar.Item>Home</Navbar.Item>
+      <Navbar.Item>Sale</Navbar.Item>
+
+      <Navbar.Item dropdown>
+        <Navbar.Link>More</Navbar.Link>
+        <Navbar.Dropdown>
+          <Navbar.Item>About</Navbar.Item>
+          <Navbar.Item>Jobs</Navbar.Item>
+          <Navbar.Item>Contact</Navbar.Item>
+          <Navbar.Divider />
+          <Navbar.Item>Report an issue</Navbar.Item>
+        </Navbar.Dropdown>
+      </Navbar.Item>
+    </Navbar.Segment>
+
+    <Navbar.Segment align="end">
+      <Navbar.Item>
+        <Button.Group>
+          <Button color="dark">
+            <strong>Sign up</strong>
+          </Button>
+          <Button color="light">Log in</Button>
+        </Button.Group>
+      </Navbar.Item>
+    </Navbar.Segment>
+  </Navbar.Menu>
+     </Navbar>
+    
+    <Column.Group vcentered multiline>
+
+      {products.map(product => 
+        <Column 
+          key={product}
+          size="one-quarter">
+            
+        <Product
+        key = {product.sku}
+        sku = {product.sku}
+        title = {product.title}
+        description = {product.description}
+        price = {product.price}/>
+        </Column>)}
+    </Column.Group>
+    </div>
   );
 };
 
---
+++ src/components/Product.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Card, Image, Media, Title, Content, Button } from "rbx";
+
+const Product = (props) => {
+    const sizes = ['XS','S','M','L']
+
+    return(
+    <Card>
+        <Card.Image>
+            <Image.Container size="1by0.01">
+                <Image src={'./data/products/' + props.sku+'_1.jpg'} />
+            </Image.Container>
+            </Card.Image>
+        <Card.Content>
+            <Media>
+                <Media.Item>
+                <Title as="p" size={4}>
+                    {props.title}
+                </Title>
+                <Title as="p" subtitle size={6}>
+                    ${props.price}
+                </Title>
+                </Media.Item>
+            </Media>
+        <Content>
+            {props.description}
+            <br />
+        </Content>
+        <Button.Group >
+            {sizes.map(size=>
+            <Button 
+                rounded >
+                {size}
+            </Button>
+            )}
+        </Button.Group>
+        </Card.Content>
+    </Card>
+    )  
+}
+
+export default Product; 
